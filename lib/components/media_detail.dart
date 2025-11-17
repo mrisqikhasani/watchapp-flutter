406a196655d19ea3100e23398dfed96683a7cb12
mrisqikhasani <mrisqikhasani@gmail.com>
2025-11-17 21:07:42 +0700

    finish reponsive detail page

---
+++ lib/components/media_detail.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:watchapp/directory/model/movie_list_model.dart';
+
+class MediaDetail extends StatelessWidget {
+  final MovieListModel movie;
+
+  const MediaDetail({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 14),
+
+        const Text(
+          "Media",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+
+        const SizedBox(height: 12),
+
+        LayoutBuilder(
+          builder: (context, constraints) {
+            double width = constraints.maxWidth;
+
+            int crossAxisCount = 1; 
+
+            if (width > 900) {
+              crossAxisCount = 3; 
+            } else if (width > 600) {
+              crossAxisCount = 2;
+            }
+
+            return GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: movie.imageUrls.take(6).length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossAxisCount,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+                childAspectRatio: 16 / 9, 
+              ),
+              itemBuilder: (context, index) {
+                final imageUrl = movie.imageUrls[index];
+
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(14),
+                  child: Image.network(imageUrl, fit: BoxFit.cover),
+                );
+              },
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:watchapp/components/cast_detail.dart';
 import 'package:watchapp/components/highlight_detail.dart';
+import 'package:watchapp/components/media_detail.dart';
 import 'package:watchapp/directory/model/movie_list_model.dart';
-
 
 class DetailPage extends StatelessWidget {
   final MovieListModel movie;
@@ -36,37 +36,7 @@ class DetailPage extends StatelessWidget {
 
               CastDetail(movie: movie),
 
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 14),
-                  const Text(
-                    "Media",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-
-                  // List Images (vertical)
-                  Column(
-                    children: movie.imageUrls.take(3).map((imageUrl) {
-                      return Container(
-                        margin: const EdgeInsets.only(bottom: 12),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(14),
-                          child: AspectRatio(
-                            aspectRatio: 16 / 9,
-                            child: Image.network(imageUrl, fit: BoxFit.cover),
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                ],
-              ),
+              MediaDetail(movie: movie),
 
               SizedBox(height: 30),
             ],
@@ -74,23 +44,13 @@ class DetailPage extends StatelessWidget {
         ),
       ),
 
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.fromLTRB(16, 10, 16, 20),
-        decoration: BoxDecoration(
-          color: Colors.transparent,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.transparent,
-              blurRadius: 10,
-              offset: Offset(0, -2),
-            ),
-          ],
-        ),
+      bottomNavigationBar: SafeArea(
+        minimum: const EdgeInsets.fromLTRB(16, 10, 16, 20),
         child: Row(
-          children: [
-            const WatchListButton(),
+          children: const [
+            Expanded(child: WatchListButton()),
             SizedBox(width: 14),
-            const FavoriteButton(),
+            Expanded(child: FavoriteButton()),
           ],
         ),
       ),
@@ -110,16 +70,35 @@ class _FavoriteButtonState extends State<FavoriteButton> {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        isFavorite ? Icons.favorite : Icons.favorite_border,
-        color: Colors.red,
-      ),
-      onPressed: () {
+    return InkWell(
+      borderRadius: BorderRadius.circular(12),
+      onTap: () {
         setState(() {
           isFavorite = !isFavorite;
         });
       },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        padding: const EdgeInsets.symmetric(vertical: 14),
+        decoration: BoxDecoration(
+          color: isFavorite ? Colors.red.withOpacity(.25) : Colors.red,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: Colors.white,
+            ),
+            const SizedBox(width: 6),
+            Text(
+              isFavorite ? "Favorited" : "Favorite",
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -137,42 +116,39 @@ class _WatchListButtonState extends State<WatchListButton> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: InkWell(
-        borderRadius: BorderRadius.circular(12),
-        onTap: () {
-          setState(() {
-            isAdded = !isAdded;
-          });
-        },
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 250),
-          padding: EdgeInsets.symmetric(vertical: 14),
-          decoration: BoxDecoration(
-            color: isAdded ? Colors.green.withValues(alpha: .25) : Colors.white10,
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(
-              color: isAdded ? Colors.greenAccent : Colors.transparent,
-              width: 1.3,
+    return InkWell(
+      borderRadius: BorderRadius.circular(12),
+      onTap: () {
+        setState(() {
+          isAdded = !isAdded;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        padding: const EdgeInsets.symmetric(vertical: 14),
+        decoration: BoxDecoration(
+          color: isAdded ? Colors.green.withOpacity(.25) : Colors.white10,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: isAdded ? Colors.greenAccent : Colors.transparent,
+            width: 1.3,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              isAdded ? Icons.check : Icons.bookmark_add_outlined,
+              color: Colors.white,
             ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                isAdded ? Icons.check : Icons.bookmark_add_outlined,
-                color: Colors.white,
-              ),
-              SizedBox(width: 6),
-              Text(
-                isAdded ? "Added to Watchlist" : "Add to Watchlist",
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          ),
+            const SizedBox(width: 6),
+            Text(
+              isAdded ? "Added to Watchlist" : "Add to Watchlist",
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
